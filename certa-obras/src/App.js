95aa770c58467a449a3d163b687554f10b4251cb
gabriel_at <gabriel-almeida-97@hotmail.com>
2021-03-02 15:01:02 -0300

    teste novo deploy

--- certa-obras/src/App.js
+++ certa-obras/src/App.js
@@ -5,7 +5,6 @@ import Header from './components/HomeComponent/Header';
 import Obras from './components/HomeComponent/Obras';
 import SideMenu from './components/HomeComponent/SideMenu';
 import HeaderObra from './components/ObrasComponent/HeaderObra';
-import ObrasDetail from './components/ObrasComponent/ObraDetail';
 
 function App() {
   return (
@@ -26,7 +25,7 @@ function App() {
                   <SideMenu/>
                   <Main>
                     <HeaderObra/>
-                    <ObrasDetail/>
+                   
                   </Main>
               </Container>
             </Route>
