c92ba1597abe88e2e354cdc54c3a2f62e342e66b
gabriel_at <gabriel-almeida-97@hotmail.com>
2021-03-02 15:06:27 -0300

    correção state url

--- certa-obras/src/components/ObrasComponent/HeaderObra.js
+++ certa-obras/src/components/ObrasComponent/HeaderObra.js
@@ -27,7 +27,7 @@ import styled from 'styled-components';
 
 
     
-      firebase.database().ref('Obras/'+this.state.url[3]).on('value', (snapshot) =>{
+      firebase.database().ref('Obras/'+this.state.url[2]).on('value', (snapshot) =>{
         let state = this.state;
         state.key = snapshot.key;
         state.nome = snapshot.val().nomeObra;
